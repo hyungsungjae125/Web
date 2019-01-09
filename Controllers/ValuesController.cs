57c9b8f643096298c9998c19d45445db35777221
형성재 <tldwo125@naver.com>
2019-01-09 17:53:02 +0900

    test

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace Web.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "valuetest1", "valuetest2" };
+            return new string[] { "valuetest123", "valuetest234" };
         }
 
         // GET api/values/5
